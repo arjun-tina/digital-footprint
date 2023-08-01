7e82dac3f3171fc026f39ffc96a751afd6ecbd3c
Arjun Chadha <arjun.chadha@duke.edu>
2023-08-01 17:09:43 -0400

    Updated popup.js, reflecting changes in popup.html and background.js

--- popup.js
+++ popup.js
@@ -1,23 +1,42 @@
-// Function to format time in HH:mm:ss format
-function formatTime(seconds) {
-    const hrs = Math.floor(seconds / 3600);
-    const mins = Math.floor((seconds % 3600) / 60);
-    const secs = Math.floor(seconds % 60);
-    return `${hrs.toString().padStart(2, "0")}:${mins
-      .toString()
-      .padStart(2, "0")}:${secs.toString().padStart(2, "0")}`;
+document.addEventListener("DOMContentLoaded", () => {
+
+  // Function to update popup
+  function updatePopup() {
+    chrome.storage.local.get(["time", "carbonEmissions"], (result) => {
+      const timeDisplay = document.getElementById("timeDisplay");
+      const emissionsDisplay = document.getElementById("emissionsDisplay");
+      
+      const totalTime = result.time || 0;
+      const hours = Math.floor(totalTime / 3600);
+      const minutes = Math.floor((totalTime % 3600) / 60);
+      const seconds = totalTime % 60;
+      const formattedTime = `${hours}h ${minutes}m ${seconds}s`;
+      timeDisplay.textContent = formattedTime;
+
+      const carbonEmissions = result.carbonEmissions || 0;
+      const formattedEmissions = `${carbonEmissions.toFixed(2)} kg CO2`;
+      emissionsDisplay.textContent = formattedEmissions;
+    });
   }
+
+  // Call updatePopup function to populate popup initially
+  updatePopup();
+
+  // Listen for changes in storage and update popup when needed
+  chrome.storage.onChanged.addListener((changes) => {
+    if (changes.time || changes.carbonEmissions) {
+      updatePopup();
+    }
+  });
+
+  // Handle reset button click event
+  const resetButton = document.getElementById("resetButton");
+  resetButton.addEventListener("click", () => {
+    chrome.runtime.sendMessage({ reset: true }, (response) => {
+      if (response.success) {
+        updatePopup(); 
+      }
+    });
+  });
   
-// Function to update pop-up with online time and carbon emissions
-function updatePopup() {
-chrome.runtime.getBackgroundPage((backgroundPage) => {
-    const { onlineTime, carbonEmissions } = backgroundPage;
-    document.getElementById("onlineTime").textContent = `Online Time: ${formatTime(onlineTime)}`;
-    document.getElementById(
-    "carbonEmissions"
-    ).textContent = `Estimated Carbon Emissions: ${carbonEmissions.toFixed(2)} kgCO2`;
 });
-}
-  
-// Call updatePopup function when the pop-up is loaded
-document.addEventListener("DOMContentLoaded", updatePopup);
